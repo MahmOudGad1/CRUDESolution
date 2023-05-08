188a8ed9dfa855bdd73bdb4bf8547c985d763a7f
Mahmoud Salah <Mahmoud.Salah@premiumcard.net>
2023-05-08 12:52:35 +0300

    Add project files.

---
+++ CRUDETests/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace CRUDETests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+             int add(int input1, int input2)
+            {
+                return input1 + input2;
+            }
+        }
+    }
+}
